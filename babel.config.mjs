a720a4e122f1018bf9a61ff3e568b2bf37ec3914
Neftaly <neftalyrv23@gmail.com>
2025-05-13 18:51:32 -0400

    configured jest and babel to test

---
+++ babel.config.mjs
@@ -0,0 +1,10 @@
+export default {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: 'node 14', // Adjust for your node version if necessary
+            },
+        ],
+    ],
+}
